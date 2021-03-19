fdd930bd0ed385432e4e2e98ee0183c184b952e6
Angel Martinez <maz.so1997@gmail.com>
2021-03-18 21:58:11 -0600

    Pagina Web

--- veterinaria-fullstack/backend/recursos.js
+++ veterinaria-fullstack/backend/recursos.js
@@ -2,7 +2,7 @@ module.exports = {
   mascotas: [
     { tipo: "Perro", nombre: "Trosky0", dueno: "Camilo" },
     { tipo: "Perro", nombre: "Trosky1", dueno: "Camilo" },
-    { tipo: "Perro", nombre: "Trosky2", dueno: "Camilo" },
+    { tipo: "Gato", nombre: "Flash", dueno: "Miguel" },
     { tipo: "Perro", nombre: "Trosky3", dueno: "Camilo" },
     { tipo: "Perro", nombre: "Trosky4", dueno: "Camilo" },
   ],
